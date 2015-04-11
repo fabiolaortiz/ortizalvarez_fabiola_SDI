5ec69af0eedd25f934680ef2ab6e4f436bcffd7e
fabiolaortiz <fmortizalvarez@fullsail.edu>
2015-04-11 18:31:16 -0400

    Expression Worksheet

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,13 @@
+//Fabiola Ortiz-Alvarez
+//April 11, 2015
+//Assignment: Expression Worksheet
+
+//Calculate dog's years
+//Asking for dog age and saving it in a variable
+var dogAge = prompt('Enter your dog age:');
+//Calculating dog's age in human years
+var humanYears = dogAge * 7;
+
+//Displays information to the user
+alert('Sparky is ' + dogAge + ' human years old which is ' + humanYears + ' in dog years.');
+
