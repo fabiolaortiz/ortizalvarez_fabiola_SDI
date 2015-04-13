1fc86b816ee5319e95284a2f59bbd67ffa0f7ec6
fabiolaortiz <fmortizalvarez@fullsail.edu>
2015-04-13 10:52:34 -0400

    Expressions Worksheet
    Average shopping bill.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -36,3 +36,15 @@ var sliceSparky = pizzaSlices * pizzaOrdered % qtyPeople;
 
 //Displays how much pizza Sparky gets
 alert('Sparky got ' + sliceSparky + ' slices of pizza.');
+
+
+//Calculate average of a shopping bill
+//Creating array with values
+var groceryArray = [150,134,169,189,125];
+
+//Calculating total amount by adding values in array and the average amount by dividing the total
+var totalAmount = groceryArray[0] + groceryArray[1] + groceryArray[2] + groceryArray[3] + groceryArray[4];
+var aveAmount = totalAmount/5;
+
+//Displays the total amount and the average amount to the user
+alert('You have spent a total of \uFF04' + totalAmount + ' on groceries over 5 weeks. \nThat is an average of \uFF04' + aveAmount + ' per week.');
