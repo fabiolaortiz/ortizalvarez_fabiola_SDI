98657aaa8377785382b1177a4033d0b5e97fe599
fabiolaortiz <fmortizalvarez@fullsail.edu>
2015-04-15 17:55:43 -0400

    Expression Assignment
    Creating variables for storing user input.

--- Ortiz-Alvarez_Fabiola_Expressions_Assignment/js/script.js
+++ Ortiz-Alvarez_Fabiola_Expressions_Assignment/js/script.js
@@ -2,3 +2,8 @@
 //April 15, 2015
 //Assignment: Expression Assignment
 
+//Obtain user input for calculation
+var speed = prompt('Please enter your speed in miles:');
+var distance = prompt('Please enter your distance in miles:');
+var time = prompt('Please enter your time in hours: \nExample: 6 hours and 30 minutes = 6.50');
+
