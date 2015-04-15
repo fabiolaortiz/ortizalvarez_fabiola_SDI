5fc531b572023b298c12fa903e1a1b47f2e92b1c
fabiolaortiz <fmortizalvarez@fullsail.edu>
2015-04-15 18:40:48 -0400

    Expression Assingment
    Performing calculation

--- Ortiz-Alvarez_Fabiola_Expressions_Assignment/js/script.js
+++ Ortiz-Alvarez_Fabiola_Expressions_Assignment/js/script.js
@@ -8,3 +8,8 @@ var interest = prompt('Please enter the interest rate in percentage:');
 var months = prompt('Please enter the loan length in months:');
 
 //Perform calculation
+interest /= 100;
+interest /= 12;
+var monthlyPmts = principal * (interest / (1 - Math.pow((1 + interest),-months)));
+
+console.log(monthlyPmts);
