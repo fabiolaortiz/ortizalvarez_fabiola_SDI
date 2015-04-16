9c7f73ac8e7462fe34dc76d6826c317363d7ec0e
fabiolaortiz <fmortizalvarez@fullsail.edu>
2015-04-16 17:16:39 -0400

    Creating alerts
    Creating alerts to display information to user.

--- Ortiz-Alvarez_Fabiola_Expressions_Assignment/js/script.js
+++ Ortiz-Alvarez_Fabiola_Expressions_Assignment/js/script.js
@@ -1,6 +1,9 @@
-//Fabiola Ortiz-Alvarez
-//April 15, 2015
-//Assignment: Expression Assignment
+/*
+Fabiola Ortiz-Alvarez
+SDI Section #1
+April 15, 2015
+Assignment: Expression Assignment
+*/
 
 //Obtain user input for calculation
 var principal1 = prompt('Please enter amount of loan:');
@@ -8,36 +11,36 @@ var interest1 = prompt('Please enter the interest rate in percentage:');
 var months1 = prompt('Please enter the loan length in months:');
 
 //Perform calculation number 1
-interest1 /= 100;
-interest1 /= 12;
-var monthlyPmts1 = principal1 * (interest1 / (1 - Math.pow((1 + interest1),-months1)));
+interestMonth1 = interest1 / 100;
+interestMonth1 /= 12;
+var monthlyPmts1 = principal1 * (interestMonth1 / (1 - Math.pow((1 + interestMonth1),-months1)));
 
 var principal2 = prompt('Please enter amount of loan:');
 var interest2 = prompt('Please enter the interest rate in percentage:');
 var months2 = prompt('Please enter the loan length in months:');
 
 //Perform calculation number 2
-interest2 /= 100;
-interest2 /= 12;
-var monthlyPmts2 = principal2 * (interest2 / (1 - Math.pow((1 + interest2),-months2)));
+interestMonth2 = interest2 / 100;
+interestMonth2 /= 12;
+var monthlyPmts2 = principal2 * (interestMonth2 / (1 - Math.pow((1 + interestMonth2),-months2)));
 
 var principal3 = prompt('Please enter amount of loan:');
 var interest3 = prompt('Please enter the interest rate in percentage:');
 var months3 = prompt('Please enter the loan length in months:');
 
 //Perform calculation number 3
-interest3 /= 100;
-interest3 /= 12;
-var monthlyPmts3 = principal3 * (interest3 / (1 - Math.pow((1 + interest3),-months3)));
+interestMonth3 = interest3 / 100;
+interestMonth3 /= 12;
+var monthlyPmts3 = principal3 * (interestMonth3 / (1 - Math.pow((1 + interestMonth3),-months3)));
 
 var principal4 = prompt('Please enter amount of loan:');
 var interest4 = prompt('Please enter the interest rate in percentage:');
 var months4 = prompt('Please enter the loan length in months:');
 
 //Perform calculation number 4
-interest4 /= 100;
-interest4 /= 12;
-var monthlyPmts4 = principal4 * (interest4 / (1 - Math.pow((1 + interest4),-months4)));
+interestMonth4 = interest4 / 100;
+interestMonth4 /= 12;
+var monthlyPmts4 = principal4 * (interestMonth4 / (1 - Math.pow((1 + interestMonth4),-months4)));
 
 // Creating array for the four different scenarios
 var arrayLoan = new Array(4);
@@ -47,4 +50,14 @@ arrayLoan[2] = monthlyPmts3;
 arrayLoan[3] = monthlyPmts4;
 
 //Displaying monthly payments for each loan
-console.log('Your monthly payment is:' + arrayLoan);
+console.log('Your monthly payment for the first scenario of \uFF04' + principal1 + ' and ' + interest1 + '\u0025 and ' + months1 + ' months is ' + arrayLoan[0]);
+alert('Your monthly payment for the first scenario of \uFF04' + principal1 + ' and ' + interest1 + '\u0025 and ' + months1 + ' months is ' + arrayLoan[0]);
+
+console.log('Your monthly payment for the second scenario of \uFF04' + principal2 + ' and ' + interest2 + '\u0025 and ' + months2 + ' months is ' + arrayLoan[1]);
+alert('Your monthly payment for the second scenario of \uFF04' + principal3 + ' and ' + interest2 + '\u0025 and ' + months2 + ' months is ' + arrayLoan[1]);
+
+console.log('Your monthly payment for the third scenario of \uFF04' + principal3 + ' and ' + interest3 + '\u0025 and ' + months3 + ' months is ' + arrayLoan[2]);
+alert('Your monthly payment for the third scenario of \uFF04' + principal3 + ' and ' + interest3 + '\u0025 and ' + months3 + ' months is ' + arrayLoan[2]);
+
+console.log('Your monthly payment for the fourth scenario of \uFF04' + principal4 + ' and ' + interest4 + '\u0025 and ' + months4 + ' months is ' + arrayLoan[3]);
+alert('Your monthly payment for the fourth scenario of \uFF04' + principal4 + ' and ' + interest4 + '\u0025 and ' + months4 + ' months is ' + arrayLoan[3]);
