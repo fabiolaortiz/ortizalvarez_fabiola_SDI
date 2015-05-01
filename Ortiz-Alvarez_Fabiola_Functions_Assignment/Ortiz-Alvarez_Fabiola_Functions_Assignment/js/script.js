600f4a5d97689cf17e12c38d5160cc46f8f31119
fabiolaortiz <fmortizalvarez@fullsail.edu>
2015-04-30 22:41:20 -0400

    Creating anonymous function
    Created anonymous function with three variables.

--- Ortiz-Alvarez_Fabiola_Functions_Assignment/Ortiz-Alvarez_Fabiola_Functions_Assignment/js/script.js
+++ Ortiz-Alvarez_Fabiola_Functions_Assignment/Ortiz-Alvarez_Fabiola_Functions_Assignment/js/script.js
@@ -5,7 +5,9 @@
  Assignment: Functions Assignment
  */
 
-var itemPrice = prompt("Let's calculate the discount on your item!\nssPlease enter the item price:");
+var itemName = prompt("Let's calculate the discount on your item!\nWhat is the item?");
+
+var itemPrice = prompt("Please enter the item price:");
 
 validatePrompts(itemPrice);
 
@@ -24,29 +26,32 @@ var discountAmt = prompt("Please enter the discount amount in percentage:");
 validatePrompts(discountAmt);
 
 
-//var totalAmt = function(discount, price, ){
+var totalAmt = function(discount, price, item){
+    discount /= 100;
+    var totalSavings = price * discount;
+    var finalAmount = price * (discount + 1);
+    var messageFinal = console.log("The price for the " + item + " is \uFF04" + price + " and the total amount is \uFF04" + finalAmount.toFixed(2) + ".\nThe total savings was of \uFF04" + totalSavings.toFixed(2) + ".");
+};
 
-//}
-
-
-
+totalAmt(discountAmt,itemPrice,itemName);
 
 
 var secondDiscount = prompt("Would you like to calculate another amount of discount?\nAnswer yes or no.");
 
 while(secondDiscount==="yes" || secondDiscount==='Yes' || secondDiscount==="no" || secondDiscount==="No"){
     if(secondDiscount==="yes" || secondDiscount==='Yes'){
+        var itemName2 = prompt("What is the item?");
         var itemPrice2 = prompt("Please enter the item price:");
         validatePrompts(itemPrice2);
         var discountAmt2 = prompt("Please enter a discount amount:");
         validatePrompts(discountAmt2);
+        totalAmt(discountAmt2,itemPrice2,itemName2);
         break;
     }else if(secondDiscount==="no" || secondDiscount==="No"){
         alert("Thank you for using the discount calculator. Have a wonderful day!");
         break;
     }
 };
-
 
 
 
