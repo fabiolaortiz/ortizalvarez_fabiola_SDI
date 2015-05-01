0a1342271babbac299f7a165df7b3c679a7e2fb0
fabiolaortiz <fmortizalvarez@fullsail.edu>
2015-04-30 22:09:40 -0400

    Functions Assingment
    Creating prompts and functions. Also, anonymous function.

--- Ortiz-Alvarez_Fabiola_Functions_Assignment/Ortiz-Alvarez_Fabiola_Functions_Assignment/js/script.js
+++ Ortiz-Alvarez_Fabiola_Functions_Assignment/Ortiz-Alvarez_Fabiola_Functions_Assignment/js/script.js
@@ -5,12 +5,51 @@
  Assignment: Functions Assignment
  */
 
-var itemPrice = prompt("Let's calculate the discount on your item!\nPlease enter the item price:");
+var itemPrice = prompt("Let's calculate the discount on your item!\nssPlease enter the item price:");
 
-while (itemPrice === "" || isNaN(itemPrice)) {
-    if (itemPrice === "") {
-        itemPrice = prompt("Please don't leave blank.\nPlease enter the item price:")
-    } else if (isNaN(itemPrice)){
-        itemPrice = prompt("You have entered a letter.\nPlease enter the item price:")
+validatePrompts(itemPrice);
+
+function validatePrompts(price) {
+    while (price === "" || isNaN(price)) {
+        if (price === "") {
+            price = prompt("Please don't leave blank.\nPlease enter the item price:");
+        } else if (isNaN(price)) {
+            price = prompt("You have entered a letter.\nPlease enter the item price:");
         }
+    }
 };
+
+var discountAmt = prompt("Please enter the discount amount in percentage:");
+
+validatePrompts(discountAmt);
+
+
+//var totalAmt = function(discount, price, ){
+
+//}
+
+
+
+
+
+var secondDiscount = prompt("Would you like to calculate another amount of discount?\nAnswer yes or no.");
+
+while(secondDiscount==="yes" || secondDiscount==='Yes' || secondDiscount==="no" || secondDiscount==="No"){
+    if(secondDiscount==="yes" || secondDiscount==='Yes'){
+        var itemPrice2 = prompt("Please enter the item price:");
+        validatePrompts(itemPrice2);
+        var discountAmt2 = prompt("Please enter a discount amount:");
+        validatePrompts(discountAmt2);
+        break;
+    }else if(secondDiscount==="no" || secondDiscount==="No"){
+        alert("Thank you for using the discount calculator. Have a wonderful day!");
+        break;
+    }
+};
+
+
+
+
+//function calculateDiscount (itemPrice, discountAmount, ){
+
+//}
