9e874875141337725699ae8b34a422a850466059
fabiolaortiz <fmortizalvarez@fullsail.edu>
2015-04-30 23:02:04 -0400

    Comments
    Adding comments to the lines of programming.

--- Ortiz-Alvarez_Fabiola_Functions_Assignment/Ortiz-Alvarez_Fabiola_Functions_Assignment/js/script.js
+++ Ortiz-Alvarez_Fabiola_Functions_Assignment/Ortiz-Alvarez_Fabiola_Functions_Assignment/js/script.js
@@ -5,12 +5,16 @@
  Assignment: Functions Assignment
  */
 
+//Defining a variable for the type of item.
 var itemName = prompt("Let's calculate the discount on your item!\nWhat is the item?");
 
+//Defining a variable for the total price of the item.
 var itemPrice = prompt("Please enter the item price:");
 
+//Calling the validation function
 validatePrompts(itemPrice);
 
+//Creating a function to validate prompts from blanks and letters.
 function validatePrompts(price) {
     while (price === "" || isNaN(price)) {
         if (price === "") {
@@ -22,11 +26,13 @@ function validatePrompts(price) {
     }
 };
 
+//Defining a variable for the amount of discount.
 var discountAmt = prompt("Please enter the discount amount in percentage:");
 
+//Calling the validation function.
 validatePrompts(discountAmt);
 
-
+//Creating an anonymous function for the calculation of the discount.
 var totalAmt = function(discount, price, item){
     discount /= 100;
     var totalSavings = price * discount;
@@ -34,24 +40,43 @@ var totalAmt = function(discount, price, item){
     return [item,price,totalSavings,finalAmount];
 };
 
-
+//Defining a variable for storing the returning array from the function of the calculation.
 var firstAmount = totalAmt(discountAmt,itemPrice,itemName);
 
+//Displaying a message to the console stating the amounts of discount, amount to pay and total savings.
 console.log("The price for the " + firstAmount[0] + " is \uFF04" + firstAmount[1] + " and the total amount is \uFF04" + firstAmount[3].toFixed(2) + ".\nThe total savings was of \uFF04" + firstAmount[2].toFixed(2) + ".");
 
+//Defining a variable for
 var secondDiscount = prompt("Would you like to calculate another amount of discount?\nAnswer yes or no.");
 
+//Creating a loop to get second information about the discount
 while(secondDiscount==="yes" || secondDiscount==='Yes' || secondDiscount==="no" || secondDiscount==="No"){
     if(secondDiscount==="yes" || secondDiscount==='Yes'){
+
+        //Defining a variable for the type of item.
         var itemName2 = prompt("What is the item?");
+
+        //Defining a variable for the total price of the item.
         var itemPrice2 = prompt("Please enter the item price:");
+
+        //Calling the validation function
         validatePrompts(itemPrice2);
+
+        //Defining a variable for the amount of discount.
         var discountAmt2 = prompt("Please enter a discount amount:");
+
+        //Calling the validation function
         validatePrompts(discountAmt2);
+
+        //Defining a variable for storing the second returning array from the function of the calculation.
         var secondAmount = totalAmt(discountAmt2,itemPrice2,itemName2);
+
+        //Displaying a message to the console stating the amounts of discount, amount to pay and total savings.
         console.log("The price for the " + secondAmount[0] + " is \uFF04" + secondAmount[1] + " and the total amount is \uFF04" + secondAmount[3].toFixed(2) + ".\nThe total savings was of \uFF04" + secondAmount[2].toFixed(2) + ".");
         break;
     }else if(secondDiscount==="no" || secondDiscount==="No"){
+
+        //Displaying a message when the user only needs one calculation.
         alert("Thank you for using the discount calculator. Have a wonderful day!");
         break;
     }
